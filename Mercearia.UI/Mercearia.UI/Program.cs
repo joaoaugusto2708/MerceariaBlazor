2e7a35f861df110dbc8d8d1c06aeacdb8bb09c63
João Augusto <joaorodriguesjf@hotmail.com>
2024-06-11 00:01:47 -0300

    Alterando para usar card do Blazorise e alterando Produtos

--- Mercearia.UI/Mercearia.UI/Program.cs
+++ Mercearia.UI/Mercearia.UI/Program.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 using Mercearia.UI.Client.Pages;
 using Mercearia.UI.Components;
 
@@ -6,8 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
