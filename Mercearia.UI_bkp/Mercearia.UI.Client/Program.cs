0c140907b8abdaa1542a56395926a134b0c635eb
Lorhan <lorhan.campos@viannasempre.com.br>
2024-06-14 22:04:27 -0300

    Alteração e exclusão da tela de produtos.

--- Mercearia.UI_bkp/Mercearia.UI.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Mercearia.UI.Client.Helpers;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
-builder.Services.AddScoped(sp => new Api("http://localhost:5285"));
-
-await builder.Build().RunAsync();
