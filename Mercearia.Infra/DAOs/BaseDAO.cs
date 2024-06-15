00936be31d7667f589f298e4fe8415935749412d
João Augusto <joaorodriguesjf@hotmail.com>
2024-06-15 14:26:47 -0300

    Subindo correção de bugs

--- Mercearia.Infra/DAOs/BaseDAO.cs
+++ Mercearia.Infra/DAOs/BaseDAO.cs
@@ -16,7 +16,7 @@ namespace Mercearia.Infra.DAO
     {
         public abstract string NomeTabela { get; }
         public abstract Mapa[] Mapas { get; }
-        protected static string GetStringConnection() => "Server=localhost;Port=3306;Database=mercearia;User=root;Password=root;";
+        protected static string GetStringConnection() => "Server=localhost;Port=3306;Database=mercearia;User=root;Password=bios1480;";
 
         public async Task InsertAsync(T obj)
         {
